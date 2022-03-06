c83dd9b968c291abea4ce0c92f4c61280c1a9270
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-03-06 21:46:47 +0600

    make square component

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Square from "./components/Square";
 import { N, SQUARE_SIZE } from "./constants";
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="inverted" />
       {new Array(12).fill(0).map((_, index) => {
-        return (
-          <View
-            key={index}
-            style={{
-              height: SQUARE_SIZE,
-              aspectRatio: 1,
-              backgroundColor: "white",
-              opacity: (index + 1) / N,
-            }}
-          />
-        );
+        return <Square key={index} index={index} />;
       })}
     </View>
   );
---
+++ components/Square.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { N, SQUARE_SIZE } from "../constants";
+
+export default function Square({ index }) {
+  return (
+    <View
+      key={index}
+      style={{
+        height: SQUARE_SIZE,
+        aspectRatio: 1,
+        backgroundColor: "white",
+        opacity: (index + 1) / N,
+      }}
+    />
+  );
+}
