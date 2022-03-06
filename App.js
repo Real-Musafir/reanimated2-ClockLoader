4e49c1ba501c5c6fde0a22f7ddde1b53ece47d18
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-03-06 20:56:35 +0600

    create 12 square

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import { N, SQUARE_SIZE } from "./constants";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>asdf</Text>
+      <StatusBar style="inverted" />
+      {new Array(12).fill(0).map((_, index) => {
+        return (
+          <View
+            key={index}
+            style={{
+              height: SQUARE_SIZE,
+              aspectRatio: 1,
+              backgroundColor: "white",
+              opacity: (index + 1) / N,
+            }}
+          />
+        );
+      })}
     </View>
   );
 }
@@ -12,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#111",
     alignItems: "center",
     justifyContent: "center",
   },
