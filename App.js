e41090f511876c95e7c06900496db177322259b8
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-03-06 22:23:37 +0600

    show the rotate of all squarec

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useSharedValue, withTiming } from "react-native-reanimated";
+import { useEffect } from "react";
 import Square from "./components/Square";
 import { N, SQUARE_SIZE } from "./constants";
 export default function App() {
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    progress.value = withTiming(2 * Math.PI, { duration: 4000 });
+  });
+
   return (
     <View style={styles.container}>
       <StatusBar style="inverted" />
       {new Array(12).fill(0).map((_, index) => {
-        return <Square key={index} index={index} />;
+        return <Square key={index} index={index} progress={progress} />;
       })}
     </View>
   );
--- components/Square.js
+++ components/Square.js
@@ -1,24 +1,36 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { N, SQUARE_SIZE } from "../constants";
+import Animated, { useAnimatedStyle } from "react-native-reanimated";
 
-export default function Square({ index }) {
+export default function Square({ index, progress }) {
   const offsetAngle = (2 * Math.PI) / N;
-  const finalAngle = offsetAngle * index;
+  const finalAngle = offsetAngle * (N - 1 - index);
+
+  const rStyle = useAnimatedStyle(() => {
+    const rotate = Math.min(finalAngle, progress.value);
+
+    return {
+      transform: [
+        { rotate: `${rotate}rad` },
+        { translateY: -index * SQUARE_SIZE },
+      ],
+    };
+  });
+
   return (
-    <View
+    <Animated.View
       key={index}
-      style={{
-        height: SQUARE_SIZE,
-        aspectRatio: 1,
-        backgroundColor: "white",
-        opacity: (index + 1) / N,
-        position: "absolute",
-        transform: [
-          { rotate: `${finalAngle}` },
-          { translateY: -index * SQUARE_SIZE },
-        ],
-      }}
+      style={[
+        {
+          height: SQUARE_SIZE,
+          aspectRatio: 1,
+          backgroundColor: "white",
+          opacity: (index + 1) / N,
+          position: "absolute",
+        },
+        rStyle,
+      ]}
     />
   );
 }
