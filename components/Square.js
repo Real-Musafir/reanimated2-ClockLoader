b2fa7ad0a4ed116c6f394cdee56e2c3af4f2c5cf
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-03-06 22:14:37 +0600

    make angle and rotate every square

--- components/Square.js
+++ components/Square.js
@@ -3,6 +3,8 @@ import React from "react";
 import { N, SQUARE_SIZE } from "../constants";
 
 export default function Square({ index }) {
+  const offsetAngle = (2 * Math.PI) / N;
+  const finalAngle = offsetAngle * index;
   return (
     <View
       key={index}
@@ -12,7 +14,10 @@ export default function Square({ index }) {
         backgroundColor: "white",
         opacity: (index + 1) / N,
         position: "absolute",
-        transform: [{ translateY: -index * SQUARE_SIZE }],
+        transform: [
+          { rotate: `${finalAngle}` },
+          { translateY: -index * SQUARE_SIZE },
+        ],
       }}
     />
   );
